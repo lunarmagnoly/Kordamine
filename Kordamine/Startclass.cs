c49db9eeb615b5303b5adc352aa1277cbe5f1dff
lunarmagnoly <mari.luna.tei@outlook.com>
2025-05-22 14:39:06 +0300

    2.osa ylesandeid tehtud

--- Kordamine/Startclass.cs
+++ Kordamine/Startclass.cs
@@ -93,11 +93,142 @@ namespace Kordamine
                 if (kliendikaart == "jah")
                 {
                     int soodustus = 30;
-                    float uus_hind = hind*((100f - soodustus)/100f);
+                    float uus_hind = hind * ((100f - soodustus) / 100f);
                     Console.WriteLine("Kliendikaardiga soodustus on {0}%. \nSoodushind on {1}.\nTäishind on {2}.", soodustus, uus_hind, hind);
                 }
 
             }
+            //Küsi temperatuur ning teata, kas see on üle kaheksateistkümne kraadi (soovitav toasoojus talvel).
+            Console.Write("\n\nSiseta toa temperatuur: ");
+            float temperatuur = float.Parse(Console.ReadLine());
+            if (temperatuur > 18 && temperatuur < 25)
+            {
+                Console.WriteLine("Temperatuur on üle kaheksateistkümne kraadi(soovitav toasoojus talvel)");
+
+            }
+            else
+            {
+                Console.WriteLine("Temperatuur on väljaspool soovituslikku vahemikku (18–25 °C).");
+            }
+
+            //Küsi inimese pikkus ning teata, kas ta on lühike, keskmine või pikk(piirid pane ise paika)
+
+            Console.Write("\n\nSiseta oma pikkus: ");
+            float pikkus = float.Parse(Console.ReadLine());
+            if (pikkus > 50 && pikkus <= 150)
+            {
+                Console.WriteLine("Te olete lühike");
+
+            }
+            else if (pikkus > 150 && pikkus <= 175)
+            {
+                Console.WriteLine("Teie pikkus on keskmine");
+            }
+            else if (pikkus > 175 && pikkus <= 220)
+            {
+                Console.WriteLine("Te olete pikk");
+            }
+            else { Console.WriteLine("Pikkus ei saa olla väiksem kui 50 cm."); }
+
+            //Küsi inimeselt pikkus ja sugu ning teata, kas ta on lühike, keskmine või pikk (mitu tingimusplokki võib olla üksteise sees).
+
+            Console.WriteLine("Sisestage oma sugu(m/n): ");
+            char sugu = char.Parse(Console.ReadLine().ToLower());
+            if (sugu == 'n')
+            {
+
+                Console.Write("\n\nSiseta oma pikkus: ");
+                pikkus = float.Parse(Console.ReadLine());
+                if (pikkus > 50 && pikkus <= 160)
+                {
+                    Console.WriteLine("Proua! \nTe olete lühike");
+
+                }
+                else if (pikkus > 160 && pikkus <= 175)
+                {
+                    Console.WriteLine("Proua! \nTeie pikkus on keskmine");
+                }
+                else if (pikkus > 175 && pikkus <= 220)
+                {
+                    Console.WriteLine("Proua! \nTe olete pikk");
+                }
+                else { Console.WriteLine("Pikkus peab olema vahemikus 50 kuni 220 cm."); }
+            }
+            else if (sugu == 'm')
+            {
+
+                Console.Write("\n\nSiseta oma pikkus: ");
+                pikkus = float.Parse(Console.ReadLine());
+                if (pikkus > 50 && pikkus <= 170)
+                {
+                    Console.WriteLine("Härra! \nTe olete lühike");
+
+                }
+                else if (pikkus > 170 && pikkus <= 185)
+                {
+                    Console.WriteLine("Härra! \nTeie pikkus on keskmine");
+                }
+                else if (pikkus > 185 && pikkus <= 220)
+                {
+                    Console.WriteLine("Härra! \nTe olete pikk");
+                }
+                else { Console.WriteLine("Pikkus peab olema vahemikus 50 kuni 220 cm."); }
+            }
+
+            // Küsi inimeselt poes eraldi kas ta soovib osta piima, saia, leiba. Löö hinnad kokku ning teata, mis kõik ostetud kraam maksma läheb.
+            Console.WriteLine("Tere tulemast poodi! \nKas soovite piima osta?(Jah/Ei)");
+            string piima_soov = Console.ReadLine().ToLower();
+            float kogu_summa = 0f;
+            //piim
+            if (piima_soov == "jah")
+            {
+                float piima_hind = 1.85f;
+                kogu_summa +=  piima_hind;
+                Console.WriteLine("Arvestame piima. \nPiima hind on {0}. \nKogusumma on {1}.", piima_hind, kogu_summa);
+                
+                
+
+            }
+            else 
+            {
+                Console.WriteLine("Edasi. \nPiima ei arvesta. \nKogusumma on {0}.",  kogu_summa);
+               
+            }
+
+            //sai
+            Console.WriteLine("Kas soovite saia osta?(Jah/Ei)");
+            string saia_soov = Console.ReadLine().ToLower();
+            if (saia_soov == "jah")
+            {
+
+                float saia_hind = 0.65f;
+                kogu_summa += saia_hind;
+                Console.WriteLine("Arvestame saia. \nSaia hind on {0}. \nKogusumma on {1}.", saia_hind, kogu_summa);
+                
+                
+
+            }
+            else
+            {
+                Console.WriteLine("Edasi. \nSaia ei arvesta. \nKogusumma on {0}.", kogu_summa);
+                
+            }
+
+            //leib
+            Console.WriteLine("Kas soovite leiba osta?(Jah/Ei)");
+            string leiba_soov = Console.ReadLine().ToLower();
+            if (leiba_soov == "jah")
+            {
+                float leiba_hind = 1.35f;
+                kogu_summa += leiba_hind;
+                Console.WriteLine("Arvestame leiba. \nLeiva hind on {0}. \nKogusumma on {1}.", leiba_hind, kogu_summa);
+
+            }
+            else
+            {
+                Console.WriteLine("Edasi. \nLeib ei arvesta. \nKogusumma on {0}.", kogu_summa);
+            }
+            Console.WriteLine("\nAitäh ostu eest! Kokku tuleb maksta: {0} €", kogu_summa);
+
         }
-    }
-}
+    } }
